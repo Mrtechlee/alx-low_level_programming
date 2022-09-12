9904ec6b8ae1175117d33ae1436593e28e0226ee
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-12 14:35:18 -0700

    Update 8-print_base16.c

--- 8-print_base16.c
+++ 8-print_base16.c
@@ -1,1 +1,26 @@
 #!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int i;
+	char k;
+
+	for (i = 48; i < 58; i++)
+	{
+
+		putchar(i);
+	}
+	for (k = 'a'; k <= 'f'; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+
+	return (0);
+}
