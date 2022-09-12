e48e381c1a08b7ad6896657fc6ce8cf75c743e67
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-12 14:20:14 -0700

    Update 101-print_comb4.c

--- 101-print_comb4.c
+++ 101-print_comb4.c
@@ -1,1 +1,34 @@
 #!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point to the program
+ *
+ * Return: 0 - Code runs successfully
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
