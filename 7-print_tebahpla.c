7b1a0a4a71027d98b0c7cb1c8d644874ca4dd7a4
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-12 14:33:08 -0700

    Update 7-print_tebahpla.c

--- 7-print_tebahpla.c
+++ 7-print_tebahpla.c
@@ -1,1 +1,18 @@
 #!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Return: Always 0 (Runs successfully)
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'z'; k >= 'a'; k--)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
