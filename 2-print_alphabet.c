3d70837790d975bf173604de89856e47c7c5333b
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-12 14:24:12 -0700

    Update 2-print_alphabet.c

--- 2-print_alphabet.c
+++ 2-print_alphabet.c
@@ -1,1 +1,18 @@
 #!/bi/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
