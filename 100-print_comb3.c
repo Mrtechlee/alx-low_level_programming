ff8f9c43163be8b7e87360a5a60bfd2f85b07936
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-12 14:18:39 -0700

    Update 100-print_comb3.c

--- 100-print_comb3.c
+++ 100-print_comb3.c
@@ -1,1 +1,34 @@
 #!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point to the program
+ *
+ * Return:  - Code runs successfully)
+ */
+
+int main(void)
+{
+	int i;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (k = 48; k < 58; k++)
+		{
+			if (i != k && i < k)
+			{
+				putchar(i);
+				putchar(k);
+				if (k == 57 && i == 56)
+				{
+					break
+						;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
