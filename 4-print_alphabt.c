0cb92ad5dd91da19af44b4dcf06455f6083a96f1
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-12 14:27:28 -0700

    Update 4-print_alphabt.c

--- 4-print_alphabt.c
+++ 4-print_alphabt.c
@@ -1,1 +1,20 @@
 #!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		if (k != 'q' && k != 'e')
+			putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
