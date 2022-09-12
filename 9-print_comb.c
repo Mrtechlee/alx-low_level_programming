224ebdb6636538174bffe479922e0d1a76481375
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-12 14:37:34 -0700

    Update 9-print_comb.c

--- 9-print_comb.c
+++ 9-print_comb.c
@@ -1,1 +1,24 @@
 #!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 - Code runs successfully
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
