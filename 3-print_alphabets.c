fea0c08a2dd02bcacacb601d0f18c596e32c3f96
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-12 14:25:50 -0700

    Update 3-print_alphabets.c

--- 3-print_alphabets.c
+++ 3-print_alphabets.c
@@ -1,1 +1,24 @@
 #!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char i;
+	char k;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (k = 'A'; k <= 'Z'; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
