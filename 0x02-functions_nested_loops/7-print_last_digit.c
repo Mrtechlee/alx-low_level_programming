cd0ba3513173ec9a2fb950967d5df74667da17cc
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-14 22:56:11 +0000

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,29 @@
-hg
+#include "main.h"
+
+/**
+ * print_last_digit - function to print last digit
+ *
+ * @n: The int to print
+ * Return: 0 for success
+ */
+
+int print_last_digit(int n)
+
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+
+}
