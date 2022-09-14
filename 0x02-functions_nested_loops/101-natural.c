ecf2785444221c25feff39cf279c3c99ef1d5283
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-14 22:41:39 +0000

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,24 @@
-kjod
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 for success
+ */
+int main(void)
+
+{
+	int k = 0;
+	int sum = 0;
+
+	while (k < 1024)
+	{
+		if (k % 3 == 0 || k % 5 == 0)
+		{
+			sum += k;
+		}
+		k++;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
