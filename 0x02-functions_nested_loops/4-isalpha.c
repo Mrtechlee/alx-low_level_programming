e19c3e7a0b24d9e945688a2d5f92d741dd6c5b6a
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-14 22:52:43 +0000

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,24 @@
-#!\bin\bash
+#include "main.h"
+
+/**
+ * _isalpha - checks if c is a letter,lowercase or uppercase
+ *
+ * @c: arg for the function _isalpha
+ * Return:  0 for success
+ */
+
+int _isalpha(int c)
+
+{
+
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
