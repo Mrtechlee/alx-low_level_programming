0f0fb2eb6006215cf4b47bba2096b5113e0253b5
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-14 22:57:41 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,30 @@
-lk
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minutes of the day of Jack Bauer
+ *
+ * Return: 0 for success
+ */
+
+void jack_bauer(void)
+
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar (i / 10 + '0');
+			_putchar (i % 10 + '0');
+			_putchar (':');
+			_putchar (j / 10 + '0');
+			_putchar (j % 10 + '0');
+			_putchar ('\n');
+
+		}
+
+	}
+
+}
