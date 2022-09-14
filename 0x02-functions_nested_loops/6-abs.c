cbef3b5cae6a5b4d6be5fba2b3c733a42c56772c
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-14 22:55:10 +0000

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,28 @@
-m
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - value absolute
+ *
+ * @n: The integer to print
+ * Return: 0 for success
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+
+	else if (n == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (n);
+	}
+
+}
