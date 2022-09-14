61f7deae09f423a8e8daa4682671f50b92abce36
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-14 22:38:23 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
-hg
+#include "main.h"
+/**
+ * print_alphabet - print a-z
+ * Return: 0 for success
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+
+	}
+_putchar('\n');
+}
