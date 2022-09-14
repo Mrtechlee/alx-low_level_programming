14d1c72aee96bfac9f486f4986dbdb9d8867284a
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-14 22:54:09 +0000

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,29 @@
-nj
+#include "main.h"
+
+/**
+ * print_sign - print sign of a number
+ *
+ * @n : number to check
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	int num;
+
+	if (n > 0)
+	{
+		num = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		num = 0;
+		_putchar('0');
+	}
+	else
+	{
+		num = -1;
+		_putchar('-');
+	}
+	return (num);
+}
