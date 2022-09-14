cc029c5d12af5b33d8e74455c94c501f03afb995
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-14 22:43:18 +0000

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,1 +1,30 @@
-jhi
+#include <stdio.h>
+
+/**
+ * main - prints fibonacci sequence
+ * Return: 0 for success
+ */
+int main(void)
+{
+	long int a, b, c, d;
+
+	b = 1;
+
+	c = 2;
+
+	for (a = 1; a <= 50; ++a)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		} else
+		{
+			printf("%ld\n", b);
+		}
+		d = b + c;
+		b = c;
+		c = d;
+	}
+
+	return (0);
+}
