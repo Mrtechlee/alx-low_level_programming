91296b0346604b3a55616b307c199d359b19b4aa
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-14 22:59:40 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,48 @@
-nhg
+#include "main.h"
+/**
+ * times_table - prints a multiplication table for 9
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if ((z / 10) == 0)
+			{
+				if (y == 0)
+				{
+					_putchar ('0');
+				}
+				if (y != 0)
+				{
+					_putchar(' ');
+					_putchar((z % 10) + '0');
+				}
+				if (y < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar ((z / 10) + '0');
+				_putchar ((z % 10) + '0');
+				if (y < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar ('\n');
+	}
+}
