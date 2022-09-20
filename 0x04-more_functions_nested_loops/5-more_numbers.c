c86f3a82088384153c5533ae320a4f41bf410655
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-20 10:13:58 -0800

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,28 @@
+#include "main.h"
 
+/**
+ * more_numbers - prints from 0 - 14 ten times
+ * Return: 0 for success
+ */
+
+void more_numbers(void)
+{
+	int k, i;
+
+	k = 0;
+
+	while (k < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + 48);
+			}
+			_putchar((i % 10) + 48);
+		}
+		_putchar('\n');
+
+		k++;
+	}
+}
