b3aff0dcf577ca092630868d338793ed45b73306
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-20 10:09:56 -0800

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+ *_isdigit - checks for digit through 0 - 9
+ *@c: parameter
+ *Return: 0 for success
+ */
 
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
