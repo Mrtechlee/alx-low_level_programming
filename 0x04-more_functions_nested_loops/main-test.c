9a88cebba9d30a720ea4d09ee3ef558e3c608c57
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-20 10:25:25 -0800

    Create main-test.c

---
+++ 0x04-more_functions_nested_loops/main-test.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	/**
+	 * char c;
+	 * c = '0';
+	 * printf("%c: %d\n", c, _isdigit(c));
+	 * c = 'a';
+	 * printf("%c: %d\n", c, _isdigit(c));
+	 */
+	
+	/**printf("%d\n", mul(98, 1024));
+	 * printf("%d\n", mul(-402, 4096));
+	 */
+
+	/*print_numbers();*/
+	
+	/*print_most_numbers();*/
+
+	more_numbers();
+
+	return (0);
+}
