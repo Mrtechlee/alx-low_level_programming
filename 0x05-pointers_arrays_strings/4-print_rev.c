4a5288b24fcd591050a18fc3d06f27ae40f51619
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-20 08:29:52 -0800

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,24 @@
+#include "main.h"
 
+/**
+ *print_rev - prints a string in reverse order followed by newline
+ *@s: param
+ *Return: 0 for success
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
