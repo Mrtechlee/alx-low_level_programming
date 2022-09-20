3f4348b0846508f0eb2baf63ce86c5f335f4d6a1
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-20 10:13:00 -0800

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * print_numbers - print numbers from 0 - 9
+ * Return: 0 for success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
