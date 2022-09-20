cfd133b9013c2da881314654b556448d109bb31d
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-20 10:30:35 -0800

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * print_most_numbers - print from 0 - 9 followed by a newline except 2 and 4
+ * Return: 0 for success
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
