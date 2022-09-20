de9b1690ec15f8b4597ff93e6785bc040131dfe8
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-20 10:10:30 -0800

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,30 @@
+#include "main.h"
 
+/**
+ * print_triangle - draws a triangle
+ * @size: parameter
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int i = 0, k, n = size - 1;
+
+	if (size > 0)
+	{
+		for (; i < size; i++)
+		{
+			for (k = 0; k < size; k++)
+			{
+				if (k < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
