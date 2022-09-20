4285b7c245f5d17d7a5d62a07f8d720a907faaf3
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-20 08:31:40 -0800

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,20 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * print_array - elements of an array of given integers
+ * @a: param 1
+ * @n: param 2
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
