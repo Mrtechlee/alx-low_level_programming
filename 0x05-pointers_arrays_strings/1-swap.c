0e24de3093a34bd564f9ccbb6340b51d37cf0a78
EDWARD LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-20 08:26:42 -0800

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,14 @@
+#include "main.h"
+/**
+ *swap_int - swap values of two variables a,b
+ *@a: - param 1
+ *@b: - param 2
+ *Return: 0 for success
+ */
 
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
