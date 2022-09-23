4e0725f22dfe6436824cc2f13fe5353a45368f6c
EDWARD GRITCODE LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-23 05:28:38 -0800

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,32 @@
+#include "main.h"
+/**
+ *_strncat -> concatenates two strings
+ *@dest: param 1
+ *@src: param 2
+ *@n: value
+ *Return: string
+ */
 
+char *_strncat(char *dest, char *src, int n)
+
+{
+int i, j;
+
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+
+dest[i] = '\0';
+
+return (dest);
+}
