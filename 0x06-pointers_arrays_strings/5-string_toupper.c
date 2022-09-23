a0e1ed5812a092c23969a7b7dd9829a089c5c578
EDWARD GRITCODE LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-23 05:34:49 -0800

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @str: string
+ * Return: returns char
+ */
 
+char *string_toupper(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+
+return (str);
+}
