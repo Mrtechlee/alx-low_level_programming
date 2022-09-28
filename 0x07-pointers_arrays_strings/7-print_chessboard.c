d23ace83dc6bf22e6384b5619ed5fd6dfbfe4f84
EDWARD GRITCODE LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-28 05:04:13 -0800

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int j, n;
+
+	for (j = 0; j < 8; j++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[j][n]);
+		_putchar('\n');
+	}
+}
