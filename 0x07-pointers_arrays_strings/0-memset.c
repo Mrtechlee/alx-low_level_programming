e38466673a6a23dd12d3987871b369dc00fb2c0e
EDWARD GRITCODE LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-09-28 04:58:21 -0800

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,22 @@
+#include "main.h"
+/**
+ *_memset -   a function taht fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
+ */
 
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+	{
+		*(s + j) = b; /*add 1 position s*/
+
+	}
+
+	return (s);
+}
