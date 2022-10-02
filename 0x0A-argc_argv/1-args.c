4e3533e4e8b40a1a7b9fcb35fa8ca8502cf1c40d
EDWARD GRITCODE LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-10-02 13:43:54 -0800

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
 
+	for (i = 0; i < argc; i++)
+	{
+		(void) argv; /*do not print argv*/
+	}
+	printf("%d\n", i - 1);
+	return (0);
+}
