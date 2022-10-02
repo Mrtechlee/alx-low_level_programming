490f8784e400a9a5b14d5441c10461184af7d248
EDWARD GRITCODE LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2022-10-02 13:46:10 -0800

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - entry Point
+ * atoi function converts a string into an int
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, result = 1;
 
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			result *= atoi(argv[i]);
+		}
+		printf("%d\n", result);
+	}
+	return (0);
+}
